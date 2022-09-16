2854fb1e10af37bc81f66fcbc75064530c54346c
yongchuang.xu <yongchuang.xu@akmmv.com>
2022-09-16 17:26:46 +0800

    test

---
+++ mynode.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const os = require('os');
+
+
+const app = express()
+
+
+
+
+let getIPAdress = function() {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+let ip = getIPAdress();
+
+
+app.listen(8123, () => {
+    console.log(`start,ip:${ip}`);
+})
+
+
+ 
+app.use(express.urlencoded({extended:false}))
+
+app.use(cors())
+
+
+const userRouter = require('./router'); 
+app.use('/', userRouter)  
+
+
+app.use(express.static('./public'))
+
+
+
+console.log('end');
+
+
