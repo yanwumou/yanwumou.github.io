40106869de0a65d14bbaa7e292caa60b7da595af
yongchuang.xu <yongchuang.xu@akmmv.com>
2022-09-26 16:29:29 +0800

    init

---
+++ src/themes/fisherman/index.tsx
@@ -0,0 +1,28 @@
+// 钓鱼佬主题
+import React from 'react';
+import { Theme } from '../interface';
+import { DefaultSoundNames, defaultSounds } from '../default';
+
+const imagesUrls = import.meta.glob('./images/*.png', {
+    import: 'default',
+    eager: true,
+});
+
+const fishes = Object.entries(imagesUrls).map(([key, value]) => ({
+    name: key.slice(9, -4),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    content: <img src={value} alt="" />,
+}));
+
+export const fishermanTheme: Theme<DefaultSoundNames> = {
+    title: '🐟鱼了个鱼🐟',
+    name: '钓鱼佬',
+    icons: fishes.map(({ name, content }) => ({
+        name,
+        content,
+        clickSound: 'button-click',
+        tripleSound: 'triple',
+    })),
+    sounds: defaultSounds,
+};
